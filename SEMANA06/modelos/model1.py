9fc80a11e30b6c8409eb5934e6cc107386590a16
fguerrerol <85894087+fguerrerol@users.noreply.github.com>
2021-07-24 23:13:24 -0500

    Modelo 1
    Modelo 1 - Sin comentarios Añadidos, Versión básica

---
+++ SEMANA06/modelos/model1.py
@@ -0,0 +1,131 @@
+"""
+Model exported as python.
+Name : modelo-1
+Group : 
+With QGIS : 31608
+"""
+
+from qgis.core import QgsProcessing
+from qgis.core import QgsProcessingAlgorithm
+from qgis.core import QgsProcessingMultiStepFeedback
+from qgis.core import QgsProcessingParameterFeatureSink
+import processing
+
+
+class Model1(QgsProcessingAlgorithm):
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSink('Autoinc_id', 'autoinc_id', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Length', 'length', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Field_calc', 'field_calc', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Output_less_than_11', 'OUTPUT_L_11', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Fix_geo', 'fix_geo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Wldsout', 'wldsout', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+
+    def processAlgorithm(self, parameters, context, model_feedback):
+        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
+        # overall progress through the model
+        feedback = QgsProcessingMultiStepFeedback(6, model_feedback)
+        results = {}
+        outputs = {}
+
+        # Correcion de geometrias defectuosas
+        alg_params = {
+            'INPUT': '/Users/apple/Documents/MEcon/Trim2/Herramientas/SEMANA06/langa.shp',
+            'OUTPUT': parameters['Fix_geo']
+        }
+        outputs['Fix-geometries'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fix_geo'] = outputs['Fix-geometries']['OUTPUT']
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+        # Añadir ID
+        alg_params = {
+            'FIELD_NAME': 'GID',
+            'GROUP_FIELDS': [''],
+            'INPUT': outputs['Fix-geometries']['OUTPUT'],
+            'SORT_ASCENDING': True,
+            'SORT_EXPRESSION': '',
+            'SORT_NULLS_FIRST': False,
+            'START': 1,
+            'OUTPUT': parameters['Autoinc_id']
+        }
+        outputs['Add_autoinc_field'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Autoinc_id'] = outputs['Add_autoinc_field']['OUTPUT']
+
+        feedback.setCurrentStep(2)
+        if feedback.isCanceled():
+            return {}
+
+        # Proceso 1
+        alg_params = {
+            'FIELD_LENGTH': 2,
+            'FIELD_NAME': 'length',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 1,
+            'FORMULA': 'length(NAME_PROP)',
+            'INPUT': outputs['Add_autoinc_field']['OUTPUT'],
+            'OUTPUT': parameters['Length']
+        }
+        outputs['Field_Calculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Length'] = outputs['Field_Calculator']['OUTPUT']
+
+        feedback.setCurrentStep(3)
+        if feedback.isCanceled():
+            return {}
+
+        # Filtro
+        alg_params = {
+            'INPUT': outputs['Field_Calculator']['OUTPUT'],
+            'OUTPUT_L_11': parameters['Output_less_than_11']
+        }
+        outputs['Entity_filter'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Output_less_than_11'] = outputs['Entity_filter']['OUTPUT_L_11']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
+
+        # Limitador
+        alg_params = {
+            'FIELD_LENGTH': 10,
+            'FIELD_NAME': 'Inm',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 2,
+            'FORMULA': '\"NAME_PROP\"',
+            'INPUT': outputs['Entity_filter']['OUTPUT_L_11'],
+            'OUTPUT': parameters['Field_calc']
+        }
+        outputs['Field_Calculator_Clone'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Field_calc'] = outputs['Field_Calculator_Clone']['OUTPUT']
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        # Remocion de columnas innecesarias
+        alg_params = {
+            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
+            'INPUT': outputs['Field_Calculator_Clone']['OUTPUT'],
+            'OUTPUT': parameters['Wldsout']
+        }
+        outputs['Remove_Fields'] = processing.run('qgis:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Wldsout'] = outputs['Remove_Fields']['OUTPUT']
+        return results
+
+    def name(self):
+        return 'modelo-1'
+
+    def displayName(self):
+        return 'modelo-1'
+
+    def group(self):
+        return ''
+
+    def groupId(self):
+        return ''
+
+    def createInstance(self):
+        return Model1()
