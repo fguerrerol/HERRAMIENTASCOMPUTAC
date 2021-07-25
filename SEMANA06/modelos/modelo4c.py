89123780157ae608167420ac388731bdff867e66
fguerrerol <85894087+fguerrerol@users.noreply.github.com>
2021-07-25 16:58:41 -0500

    Comentarios part 2
    Adicion de comentarios

--- SEMANA06/modelos/modelo4c.py
+++ SEMANA06/modelos/modelo4c.py
@@ -18,6 +18,11 @@ Group :
 With QGIS : 31608
 """
 
+# En esta sección se importa/convoca a los modulos necesarios para hacer las operaciones
+# con Qgis, básicamente tenemos la familia de procesamiento y el sistema de coordinadas 
+# de referencia, utilizamos la funcion from para solo invocar los módulos especificos 
+# de una librería
+
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
@@ -29,6 +34,8 @@ import processing
 class Model4c(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
+        
+        #En esta sección el programa hace el link/nexo entre variables que van a ser usadas por su procesador
         self.addParameter(QgsProcessingParameterFeatureSink('Areas_out', 'areas_out', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Countries_fixgeo', 'countries_fixgeo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Countries_reprojected', 'countries_reprojected', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
@@ -41,7 +48,9 @@ class Model4c(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Quitar campo(s) - countries_drop_fields
+        # En esta sección removemos las columasn innecesarias con el comando específico de "qgis:deletecolumn"
+        # al cual le pasamos como argumento el diccionario el cual momentaneamente tiene como keys
+        # las columnas a droppear , el shapefile al cual se le van a remover las columnaas y la ruta del objeto modificado
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','APCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','GDP_MD_EST','POP_YEAR','LASTCENSUS','GDP_YEAR','ECONOMY','INCOME_GRP','WIKIPEDIA','FIPS_10_','ISO_A2','ISO_A3_EH','ISO_N3','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_A3_IS','ADM0_A3_US','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FR','NAME_EL','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_VI','NAME_ZH','MAPCOLOR9','ADMIN_2','ISO_A3_2'],
             'INPUT': '/Users/apple/Documents/MEcon/Trim2/Herramientas/SEMANA06/data/ne_10m_admin_0_countries.shp',
